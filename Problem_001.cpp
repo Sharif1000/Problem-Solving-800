0b7854a7860234324275f45aba6647032f0bb353
Md Shariful islam <103664786+Sharif1000@users.noreply.github.com>
2025-04-08 19:17:28 +0600

    Create Problem_001.cpp

---
+++ Problem_001.cpp
@@ -0,0 +1,18 @@
+--Problem Link: https://codeforces.com/contest/1635/problem/A
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int t; cin>>t;
+    while(t--){
+      int n; cin>>n;
+      long long int sum = 0;
+      for(int i = 0; i < n; i++){
+        int x; cin>>x;
+        sum |=x;
+      }
+      cout<<sum<<endl;
+    }
+    return 0;
+}
