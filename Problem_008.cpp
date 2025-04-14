7a4209d64484c8c9c7ce64c5eb013d86346069f7
Md Shariful islam <103664786+Sharif1000@users.noreply.github.com>
2025-04-15 00:15:25 +0600

    Update Problem_008.cpp

--- Problem_008.cpp
+++ Problem_008.cpp
@@ -1,34 +1,17 @@
 --Problem Link: https://codeforces.com/contest/1950/problem/C
 
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
 int main() {
-    int t; cin >> t;
-    while (t--) {
-        string s; cin>>s;
-        int h = 0;
-        h +=(int(s[0]) - 48) * 10;
-        h +=int(s[1]) - 48;
-
-
-        if(h < 12 && h > 0){
-          cout<<s<<" AM"<<endl;
-        }
-        else if(h == 0){
-          s[0] = '1';
-          s[1] = '2';
-          cout<<s<<" AM"<<endl;
-        }
-        else if(h == 12){
-          cout<<s<<" PM"<<endl;
-        }
-        else if(h > 12 && h < 22){
-          cout<<0<<(h-12)<<":"<<s[3]<<s[4]<<" PM"<<endl;
-        }
-        else{
-          cout<<(h-12)<<":"<<s[3]<<s[4]<<" PM"<<endl;
-        }
-    }
-    return 0;
+  int t; cin>>t;
+  while(t--){
+    int h, m; 
+    char c;
+    cin >> h >> c >> m;
+    string am = (h < 12 ? " AM" : " PM");
+    h = (h % 12 ? h % 12 : 12);
+    cout << (h < 10 ? "0" : "") << h << c << (m < 10 ? "0" : "") << m << am << '\n';
+  }
+	return 0;
 }
