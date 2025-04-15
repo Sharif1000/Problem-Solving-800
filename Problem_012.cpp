c1aa59512fb9e03b7a4fc180189afd8951ccecef
Md Shariful islam <103664786+Sharif1000@users.noreply.github.com>
2025-04-15 19:58:35 +0600

    Create Problem_012.cpp

---
+++ Problem_012.cpp
@@ -0,0 +1,14 @@
+--Problem Link: https://codeforces.com/contest/1985/problem/B
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t; cin>>t;
+    while(t--){
+      int a; cin>>a;
+      if(a == 3) cout<<3<<endl;
+      else cout<<2<<endl;
+    }   
+    return 0;
+}
