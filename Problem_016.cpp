a418e4ef702f7e7630ac2a2955430b4f8811278f
Md Shariful islam <103664786+Sharif1000@users.noreply.github.com>
2025-04-23 19:22:44 +0600

    Create Problem_016.cpp

---
+++ Problem_016.cpp
@@ -0,0 +1,13 @@
+--Problem Link: https://codeforces.com/contest/1665/problem/A
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t; cin>>t;
+    while(t--){
+        int n; cin>>n;
+        cout<<n-3<< " "<<1<<" "<<1<<" "<<1<<endl;
+    }   
+    return 0;
+}
